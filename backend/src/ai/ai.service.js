7ea9e041e12d978322f6b8131213a898b89693de
Amrit kumar Mahto <amrit.mahto@adypu.edu.in>
2025-11-18 01:33:29 +0530

    gemini working fix in backend

--- backend/src/ai/ai.service.js
+++ backend/src/ai/ai.service.js
@@ -1,17 +1,17 @@
 const axios = require("axios");
 
 async function generateAIResponse(subject, topic, mode) {
-const prompt = `Generate ${mode} questions for:
-Subject: ${subject}
-Topic: ${topic}
-`;
+  const prompt = `Generate ${mode} questions for:
+  Subject: ${subject}
+  Topic: ${topic}
+  `;
 
-const url =
-    "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" +
-process.env.GEMINI_API_KEY;
+  const url =
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
+    process.env.GEMINI_API_KEY;
 
-const body = {
-contents: [
+  const body = {
+    contents: [
       {
         parts: [{ text: prompt }]
       }
