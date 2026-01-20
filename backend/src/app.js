487f1ef3991e9c0b0c6204ac04a96fa42ffedd4e
Anuj Chhajed <anujchhajed@Anujs-MacBook-Pro-2.local>
2026-01-20 20:05:48 +0530

    Fix backend for Render deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,22 @@
 require('dotenv').config()
-console.log("DB HOST:", process.env.DATABASE_URL);
 const express = require('express')
 const cors = require("cors")
 const authRoutes = require("./auth/auth.route")
 const interviewRoutes = require("./interview/interview.route")
 const mcqRoutes = require("./mcq/mcq.router.js");
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://jaago-guru-ke77.vercel.app"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use("/api/interview", interviewRoutes)
@@ -18,6 +26,6 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({success: false, message: err.message || 'Something went wrong!', data: null})
 })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
